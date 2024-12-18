91d7bc5367b97f11e364f03e43960470e493f16c
Prajwal <pajjuhulikoppe@gmail.com>
2024-12-18 11:13:07 +0530

    Added user login functionality

--- Ecommerce/src/com/ecommerce/console/userinterface/ECommerceApplication.java
+++ Ecommerce/src/com/ecommerce/console/userinterface/ECommerceApplication.java
@@ -1,8 +1,11 @@
 package com.ecommerce.console.userinterface;
 
+import java.util.List;
 import java.util.Scanner;
 
+
 import com.ecommerce.database.UserDAOImpl;
+
 import com.ecommerce.model.User;
 
 public class ECommerceApplication {
@@ -12,7 +15,8 @@ public class ECommerceApplication {
         System.out.println("Welcome to E-Commerce Application");
         
         System.out.println("1. Register User");
-        System.out.println("2. Exit");
+        System.out.println("2. Login");
+        System.out.println("3. View Products");
         
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
@@ -34,9 +38,21 @@ public class ECommerceApplication {
             user.setMobile(sc.nextLine());
             System.out.println("Enter City: ");
             user.setCity(sc.nextLine());
-            user.setRole("User");
+            user.setRole("user");
 
             userDAO.registerUser(user);
+        }else if (choice == 2) {
+            System.out.println("Enter Username: ");
+            String username = sc.nextLine();
+            System.out.println("Enter Password: ");
+            String password = sc.nextLine();
+
+            if (userDAO.loginUser(username, password)) {
+                System.out.println("Login successful!");
+                // Navigate to user operations here
+            } else {
+                System.out.println("Invalid credentials. Please try again.");
+            }
         }
     }
 }
